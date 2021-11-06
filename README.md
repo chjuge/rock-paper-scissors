97e1080ac38e97e7dadfd585dededc9fbcd02760
Ruslan (chjuge) Kumitskiy <ruslankum2@gmail.com>
2021-11-07 01:49:29 +0300

    ui is ready

--- README.md
+++ README.md
@@ -1,1 +1,8 @@
 # rock-paper-scissors
+
+Play up to 5 wins
+
+### svg images
+
+are taken from https://www.svgrepo.com/
+
--- script.js
+++ script.js
@@ -1,7 +1,71 @@
 const LOSE = "You Lose! ";
 const WIN = "You Win! ";
-const TIE = "Tie! "
+const TIE = "Tie! Both players selection is "
 
+const labelResult = document.querySelector(".round-result");
+const scoreTable = document.querySelector(".score");
+const btnPlayAgain = document.querySelector(".play-again");
+const buttons = Array.from(document.querySelectorAll(".buttons-container__button"));
+const btnsContainer = document.querySelector(".buttons-container");
+const title = document.querySelector(".make-choice");
+const playerScore = document.querySelector(".your-score");
+const oppsScore = document.querySelector(".opps-score");
+const scoreHeader = document.querySelector(".score__header");
+const restartBtn = document.querySelector(".play-again");
+const divLose = document.querySelector(".icon-holder-lose");
+const divWin = document.querySelector(".icon-holder-win");
+
+buttons.forEach(button => {
+    button.addEventListener("click",
+        () => playRound(button.textContent.trim(), computerPlay()))
+});
+
+function addHidden(...elems) {
+    elems.forEach(elem =>
+        elem.classList.add("hidden"));
+}
+
+function removeHidden(...elems) {
+    elems.forEach(elem =>
+        elem.classList.remove("hidden"));
+}
+
+function addOnePoint(elem) {
+    if (scoreTable.classList[1])
+        removeHidden(scoreTable);
+    elem.value = (parseInt(elem.value) + 1).toString();
+    if (elem.value == 5)
+        gameOver();
+}
+
+function changeLabel(text) {
+    if (labelResult.classList[1])
+        removeHidden(labelResult);
+    labelResult.value = text;
+}
+
+function gameOver() {
+    let winner = playerScore.value > oppsScore.value ? WIN : LOSE;
+    changeLabel(`Game over! ${winner}`);
+    scoreHeader.textContent = "Final Score:";
+    addHidden(btnsContainer, title);
+    if (oppsScore.value == "5")
+        removeHidden(restartBtn, divLose);
+    else
+        removeHidden(restartBtn, divWin);
+        
+}
+
+function playAgain() {
+    oppsScore.value = "0";
+    playerScore.value = "0";
+    scoreHeader.textContent = "Score:";
+    addHidden(scoreTable, labelResult, restartBtn, divLose, divWin);
+
+    removeHidden(btnsContainer, title);
+}
+
+restartBtn.addEventListener("click", playAgain);
 
 function computerPlay() {
     const tmp = Math.random();
@@ -12,62 +76,57 @@ function playRound(playerSelection, computerSelection) {
     playerSelection = playerSelection.toLowerCase();
     computerSelection = computerSelection.toLowerCase();
     switch (computerSelection) {
-        case "scissors": switch (playerSelection) {
-            case "scissors":
-                console.log(TIE + `Both players selection is ${playerSelection}`);
-                return 0;
-            case "paper":
-                console.log(LOSE + `${computerSelection} beats ${playerSelection}`);
-                return -1;
-            case "rock":
-                console.log(WIN + `${playerSelection} beats ${computerSelection}`);
-                return 1;
-            default:
-                return;
-        }
-        case "paper": switch (playerSelection) {
-            case "paper":
-                console.log(TIE + `Both players selection is ${playerSelection}`);
-                return 0;
-            case "rock":
-                console.log(LOSE + `${computerSelection} beats ${playerSelection}`);
-                return -1;
-            case "scissors":
-                console.log(WIN + `${playerSelection} beats ${computerSelection}`);
-                return 1;
-            default:
-                return;
-        }
-        case "rock": switch (playerSelection) {
-            case "rock":
-                console.log(TIE + `Both players selection is ${playerSelection}`);
-                return 0;
-            case "scissors":
-                console.log(LOSE + `${computerSelection} beats ${playerSelection}`);
-                return -1;
-            case "paper":
-                console.log(WIN + `${playerSelection} beats ${computerSelection}`);
-                return 1;
-            default:
-                return;
-        }
-        default: return;
+        case "scissors":
+            switch (playerSelection) {
+                case "scissors":
+                    changeLabel(TIE + `${playerSelection}`);
+                    break;
+                case "paper":
+                    changeLabel(LOSE + `${computerSelection} beats ${playerSelection}`);
+                    addOnePoint(oppsScore);
+                    break;
+                case "rock":
+                    changeLabel(WIN + `${playerSelection} beats ${computerSelection}`);
+                    addOnePoint(playerScore);
+                    break;
+                default:
+                    break;
+            }
+            break;
+        case "paper":
+            switch (playerSelection) {
+                case "paper":
+                    changeLabel(TIE + `${playerSelection}`);
+                    break;
+                case "rock":
+                    changeLabel(LOSE + `${computerSelection} beats ${playerSelection}`);
+                    addOnePoint(oppsScore);
+                    break;
+                case "scissors":
+                    changeLabel(WIN + `${playerSelection} beats ${computerSelection}`);
+                    addOnePoint(playerScore);
+                    break;
+                default:
+                    break;
+            }
+            break;
+        case "rock":
+            switch (playerSelection) {
+                case "rock":
+                    changeLabel(TIE + `${playerSelection}`);
+                    break;
+                case "scissors":
+                    changeLabel(LOSE + `${computerSelection} beats ${playerSelection}`);
+                    addOnePoint(oppsScore);
+                    break;
+                case "paper":
+                    changeLabel(WIN + `${playerSelection} beats ${computerSelection}`);
+                    addOnePoint(playerScore);
+                    break;
+                default:
+                    break;
+            }
+            break;
+        default: break;
     }
 }
-
-function game(rounds = 5) {
-    let score_human = 0;
-    let score_bot = 0;
-    for (i = 0; i < rounds;) {
-        let bot = computerPlay();
-        let human = prompt();
-        let res = playRound(human, bot);
-        if (res == 0)
-            continue;
-        res > 0 ? score_human++ : score_bot++;
-        i++;
-    }
-    console.log(`${score_human > score_bot ? "You Win!" : "You loose!"} Your final ${rounds} rounds score is ${score_human}`);
-}
-
-game();
